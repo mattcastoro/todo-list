05664e4cefb466e3f9c8c58bab6409bb4ddc66da
Matt Castoro <matthewjcastoro@gmail.com>
2025-01-09 11:48:22 -0700

    Replace icon images and accessibility focusing
    Remove and replace previous icons due to improper formatting.  Add
    new icons that are center justified/aligned.  Update focus color to
    style green across the board.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./style.css";
 import "./template.html";
+
+const showAddList = document.getElementById("show--add-list-dialog");
+const displayAddList = document.getElementById("dialog--new-list");
+
+showAddList.addEventListener("click", () => {
+    displayAddList.showModal();
+});
