a052f968224c066cf5124232c26296457e549753
Matt Castoro <matthewjcastoro@gmail.com>
2025-01-10 16:34:14 -0700

    Refactor CSS/HTML Classes and IDs
    Consolidate and remove redunant and ill-written classess and IDs.
    Update selectors to better account for specificity.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import "./style.css";
 import "./template.html";
 
-const showAddList = document.getElementById("show--add-list-dialog");
-const displayAddList = document.getElementById("dialog--new-list");
-
-showAddList.addEventListener("click", () => {
-    displayAddList.showModal();
+document.addEventListener("DOMContentLoaded", function() {
+    const formControls = document.querySelectorAll(".fc");
+    formControls.forEach(function(fc) {
+        fc.addEventListener("click", function(e) {
+            console.log(e.target.id);
+            distributeEventId(e.target.id)
+        });
+    });
 });
+
+function distributeEventId (id) {
+    const displayAddList = document.getElementById("dialog--new-list");
+    const displayAddTodo = document.getElementById("dialog--new-todo");
+    if (id == "show-add-list") {
+        displayAddList.showModal();
+    } else if (id == "cancel-add-list") {
+        displayAddList.close();
+    } else if (id == "show-add-todo") {
+        displayAddTodo.showModal();
+    } else if (id == "cancel-add-todo") {
+        displayAddTodo.close();
+    }
+}
