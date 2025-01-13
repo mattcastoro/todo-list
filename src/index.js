4c0980f02906391d8fc7987a4867ab3280b3af48
Matt Castoro <matthewjcastoro@gmail.com>
2025-01-13 15:04:15 -0700

    Add global event listener and event distributor
    Set the structure and base functionality of the event listener across
    all user form controls.  Pass the id data captured from the listener
    to the distributor for further functionality to be written.

--- src/index.js
+++ src/index.js
@@ -13,14 +13,44 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function distributeEventId (id) {
     const displayAddList = document.getElementById("dialog--new-list");
+    const displayDeleteList = document.getElementById("dialog--delete-list");
     const displayAddTodo = document.getElementById("dialog--new-todo");
-    if (id == "show-add-list") {
-        displayAddList.showModal();
-    } else if (id == "cancel-add-list") {
-        displayAddList.close();
-    } else if (id == "show-add-todo") {
-        displayAddTodo.showModal();
-    } else if (id == "cancel-add-todo") {
-        displayAddTodo.close();
+    const displayEditTodo = document.getElementById("dialog--edit-todo");
+    const displayDeleteTodo = document.getElementById("dialog--delete-todo");
+    switch (id) {
+        case "show-add-list":
+            displayAddList.showModal();
+            break;
+        case "add-add-list":
+            displayAddList.close();
+            break;
+        case "cancel-add-list":
+            displayAddList.close();
+            break;
+        case "show-delete-list":
+            displayDeleteList.showModal();
+            break;
+        case "cancel-delete-list":
+            displayDeleteList.close();
+            break;
+        case "show-add-todo":
+            displayAddTodo.showModal();
+            break;
+        case "cancel-add-todo":
+            displayAddTodo.close();
+            break;
+        case "show-edit-todo":
+            displayEditTodo.showModal();
+            break;
+        case "cancel-edit-todo":
+            displayEditTodo.close();
+            break;
+        case "show-delete-todo":
+            displayDeleteTodo.showModal();
+            break;
+        case "cancel-delete-todo":
+            displayDeleteTodo.close();
+            break;
     }
 }
+
