7b72e17913d354e1474bb1a503496e82530f06ae
Matt Castoro <matthewjcastoro@gmail.com>
2025-01-30 11:55:18 -0700

    Add favicon and update button image sizes
    Add favicon.  Update button images to be a little bit smaller.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
+            favicon: "./src/assets/favicon--list.svg",
         }),
     ],
     module: {
